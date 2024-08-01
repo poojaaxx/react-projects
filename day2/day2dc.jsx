0ca7718ea93d71ac5fbace9a5dd8f9629044759f
poojaaxx <150768500+poojaaxx@users.noreply.github.com>
2024-08-01 06:58:04 +0530

    Add files via upload

---
+++ day2/day2dc.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Car(){
+  return (
+    <div>
+        <center>
+      <h1>Car and Location</h1>
+      <h2>Tesla Model S</h2>
+      <img src="/car_image.png" alt="" />
+      <br></br>
+      <br></br>
+      <h2>Tesla</h2>
+      <h4>Palo Alto</h4>
+      </center>
+    </div>
+  );
+};
+
+export default Car;
