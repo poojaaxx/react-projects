7551321b037a1a8d6136de25b01fba35bf92d36a
poojaaxx <150768500+poojaaxx@users.noreply.github.com>
2024-08-07 19:04:31 +0530

    Add files via upload

---
+++ day1/day1cw3.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function World()
+{
+    return(
+        <div>
+            <h1>This is World Functional Component</h1>
+        </div>
+    )
+};
+export default World;
