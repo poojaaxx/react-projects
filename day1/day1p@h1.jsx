a7d30827d5756324c7e36929cb33ed44b4d4ec24
poojaaxx <150768500+poojaaxx@users.noreply.github.com>
2024-08-07 19:07:51 +0530

    Add files via upload

---
+++ day1/day1p@h1.jsx
@@ -0,0 +1,13 @@
+
+import React from "react";
+import World from "./day1cw3";
+function Team()
+{
+    return(
+        <div>
+            <h1>This is Team Functional Component</h1>
+            <World/>
+        </div>
+    )
+};
+export default Team;
